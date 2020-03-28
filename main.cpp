8fd533ada25d5c6d153f0ec4e41a7036e86d788b
LouisW465 <louisw200064@gmail.com>
2020-03-28 15:02:29 +0000

    Implemented JSON parser, JSON file and main/admin menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "menu.h"
 #include "accounts.h"
 #include "books.h"
+#include "jsoncpp/json/json.h"
 
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
+
+    ifstream accountFile("account.json");
+    Json::Reader reader;
+    Json::Value root;
+    reader.parse(accountFile, root);
 
     string username;
     string password;
@@ -13,25 +23,49 @@ int main() {
     /* Clears console before program writes to it*/
     printf("\033c");    
 
-    cout << "\n\t-LIBRARY MANAGEMENT SYSTEM-" << "\n\n";       
-    cout << " Are you a Student or an Admin: ";
+    cout << "\n\t-LIBRARY MANAGEMENT SYSTEM LOGIN-" << "\n\n";       
+    cout << " Are you a student or an admin: ";
     cin >> a;
-    if (a == "Student") {
+    if (a == "Student" || a == "student") {
         printf("\033c");   
         cout << " Please enter your library account username and password." << "\n\n";
         cout << " Username: ";
         cin >> username;
         cout << " Password: ";
         cin >> password;
-    } else if (a == "Admin") {
+        string correctUsr, correctPswd;
+
+        for (int i = 0; i < root["accounts"].size(); i++) {
+            correctUsr = root["accounts"][i]["username"].asString();
+            correctPswd = root["accounts"][i]["password"].asString();
+
+            if (username == correctUsr && password == correctPswd) {
+                menu m;
+                m.mainMenu(username);
+            }
+        }   
+
+    } else if (a == "Admin" || a == "admin") {
         printf("\033c");   
         cout << " Please enter your admin account username and password." << "\n\n";
         cout << " Username: ";
         cin >> username;
         cout << " Password: ";
         cin >> password;
+        string correctUsr, correctPswd, isAdmin;
+
+        for (int i = 0; i < root["accounts"].size(); i++) {
+            correctUsr = root["accounts"][i]["username"].asString();
+            correctPswd = root["accounts"][i]["password"].asString();
+            isAdmin = root["accounts"][i]["isadmin"].asString();
+            if (username == correctUsr && password == correctPswd && isAdmin == "true") {
+                menu m;
+                m.adminMenu(username);
+            }
+        }   
+
     } else {
-        cout << "ERROR: Please enter correct input. Restartng ..." << endl;
+        cout << "ERROR: Please enter either student or admin. Restartng ..." << endl;
         usleep(1000000);
         main();
     }
--- menu.h
+++ menu.h
@@ -9,16 +9,28 @@ private:
     string password;
     
 public:
-    void mainMenu();
-    void adminMenu();
+    void mainMenu(string usr);
+    void adminMenu(string usr);
 };
 
-void menu::mainMenu() {
-    cout << "---------------------------" << endl;
-    cout << " LIBRARY MANAGEMENT SYSTEM" << endl;
-    cout << "---------------------------" << endl;
+void menu::mainMenu(string usr) {
+    string a;
+    printf("\033c");  
+    cout << "\n\t---------------------------" << endl;
+    cout << "\t LIBRARY MANAGEMENT SYSTEM" << endl;
+    cout << "\t---------------------------\n\n" << endl;
+    cout << " Welcome: " << usr << "\n\n";
+    cout << "Would you like to:\n\t1.Loan a book.\n\t2.Return a book.\n\t3.Search for a book.\n\n";
+    cin >> a;
 }
 
-void menu::adminMenu() {
-
+void menu::adminMenu(string usr) {
+    string a;
+    printf("\033c");  
+    cout << "\n\t--------------------" << endl;
+    cout << "\t LIBRARY ADMIN PANEL" << endl;
+    cout << "\t--------------------\n\n" << endl;
+    cout << " Welcome: " << usr << "\n\n";
+    cout << "Would you like to:\n\t1.Add an account.\n\t2.Delete an account.\n\t3.Edit an accounts details.\n\n";
+    cin >> a;
 }
